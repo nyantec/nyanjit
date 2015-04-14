acca2fcf7c85401b560f27ce6aeaff37020d0796
Mikael Voss <mvs@nyantec.com>
2015-04-14 02:17:52 +0200

    Construct cache directory argument

--- nyanjit.c
+++ nyanjit.c
@@ -14,12 +14,9 @@
 #define XDG_SUFFIX "/" SELF
 #define HOME_SUFFIX "/.cache" XDG_SUFFIX
 
-static char buf[PATH_MAX + 1];
+#define CACHE_OPT "-object-cache-dir="
 
-static char *const argv_ext[] = {
-	"-use-mcjit",
-	"--"
-};
+static char buf[PATH_MAX + sizeof CACHE_OPT] = CACHE_OPT;
 
 static int concat(char *restrict buf,
 	char const *restrict prefix, char const *restrict suffix, size_t size) {
@@ -44,13 +41,13 @@ exit:
 	return ret;
 }
 
-static char const *cache_dir_path() {
+static char const *cache_dir_path(char *restrict buf, size_t size) {
 	char const *ret = (char *) 0;
 
 	char const *env = getenv("NYANJIT_CACHE_DIR");
 	if (env) {
 		size_t len = strlen(env);
-		if (unlikely(len >= sizeof buf))
+		if (unlikely(len >= size))
 			goto exit;
 
 		memcpy(buf, env, len + 1);
@@ -61,7 +58,7 @@ static char const *cache_dir_path() {
 
 	char const *xdg = getenv("XDG_CACHE_HOME");
 	if (xdg) {
-		if (unlikely(concat(buf, xdg, XDG_SUFFIX, sizeof buf)))
+		if (unlikely(concat(buf, xdg, XDG_SUFFIX, size)))
 			goto exit;
 
 		ret = buf;
@@ -70,7 +67,7 @@ static char const *cache_dir_path() {
 
 	char const *home = getenv("HOME");
 	if (home) {
-		if (unlikely(concat(buf, home, HOME_SUFFIX, sizeof buf)))
+		if (unlikely(concat(buf, home, HOME_SUFFIX, size)))
 			goto exit;
 
 		ret = buf;
@@ -81,10 +78,10 @@ exit:
 	return ret;
 }
 
-static char const *cache_dir() {
+static char const *cache_dir(char *restrict buf, size_t size) {
 	char const *ret = (char *) 0;
 
-	char const *path = cache_dir_path();
+	char const *path = cache_dir_path(buf, size);
 	if (unlikely(!path))
 		goto exit;
 
@@ -129,27 +126,29 @@ int main(int argc, char *argv[]) {
 	char const *path = (char *) 0;
 
 	if (!getenv("NYANJIT_CACHE_DISABLE"))
-		path = cache_dir();
+		path = cache_dir(buf + sizeof CACHE_OPT - 1,
+			sizeof buf - sizeof CACHE_OPT);
 
-	size_t argc_ext = sizeof argv_ext / sizeof *argv_ext;
-	size_t argc_lli = argc + argc_ext + 1;
+	size_t argc_lli = argc + (path ? 3 : 2);
+	size_t argc_off = 0;
 
 	/* Construct argument vector */
 	char *argv_lli[argc_lli];
-	argv_lli[0] = "NULL";
+	argv_lli[argc_off++] = "lli";
+	argv_lli[argc_off++] = "-use-mcjit";
 
-	/* TODO: Cache directory option */
+	/* Cache directory option */
+	if (path)
+		argv_lli[argc_off++] = buf;
 
-	/* Extra arguments */
-	for (size_t itr = 0; itr < argc_ext; ++itr)
-		argv_lli[itr + 1] = argv_ext[itr];
+	argv_lli[argc_off++] = "--";
 
 	/* Copy command‐line arguments */
 	for (size_t itr = 0; itr < argc; ++itr)
-		argv_lli[itr + argc_ext + 1] = argv[itr + 1];
+		argv_lli[argc_off++] = argv[itr + 1];
 
 	/* Terminate argument vector */
-	argv_lli[argc_lli - 1] = (char *) 0;
+	argv_lli[argc_off] = (char *) 0;
 
 	if (unlikely(execvp("lli", argv_lli)))
 		fprintf(stderr,
